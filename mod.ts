beb90960f67bb0bf813ca63cfee10fcb7cb8d203
Shubham Jain <Shubhamjain2908@gmail.com>
2020-06-14 02:14:18 +0530

    getting shortest & longest orbital period

--- mod.ts
+++ mod.ts
@@ -1,26 +1,26 @@
 import { parse } from "https://deno.land/std/encoding/csv.ts";
 import { BufReader } from "https://deno.land/std/io/bufio.ts";
 import { join } from "https://deno.land/std/path/mod.ts";
-import { pick } from "https://deno.land/x/lodash@4.17.15-es/lodash.js";
 
 interface Planet {
   [key: string]: string;
 }
 
-async function loadPlanetsData() {
+async function loadPlanetDataPeriods() {
   const path = join(".", "kepler_exoplanets_nasa.csv");
 
   const file = await Deno.open(path);
   const bufReader = new BufReader(file);
+
   const result = await parse(bufReader, {
     header: true,
     comment: "#",
   });
 
-  // The operating system has reached a maximum amount of open files and won't let us open anymore. Our program crashes! To solve this, we need to make sure we call Deno.close() after every Deno.open().
+  // Close file resource id (rid) to avoid leaking resources.
   Deno.close(file.rid);
 
-  const planets = (result as Array<Planet>).filter((planet: Planet) => {
+  const planets = (result as Array<Planet>).filter((planet) => {
     const planetaryRadius = Number(planet["koi_prad"]);
     const stellarRadius = Number(planet["koi_srad"]);
     const stellarMass = Number(planet["koi_smass"]);
@@ -30,17 +30,12 @@ async function loadPlanetsData() {
       stellarRadius > 0.99 && stellarRadius < 1.01 &&
       stellarMass > 0.78 && stellarMass < 1.04;
   });
-  return planets.map((planet) =>
-    pick(planet, [
-      "kepler_name",
-      "koi_prad",
-      "koi_smass",
-      "koi_srad",
-      "koi_count",
-      "koi_steff",
-    ])
-  );
+
+  return planets.map((planet) => Number(planet["koi_period"]));
 }
 
-const newEarths = await loadPlanetsData();
-console.log(`${newEarths.length} habitable planets found!`);
+const newEarthPeriods = await loadPlanetDataPeriods();
+const minPeriod = Math.min(...newEarthPeriods);
+const maxPeriod = Math.max(...newEarthPeriods);
+console.log(`${minPeriod} days was the shortest orbital period found!`);
+console.log(`${maxPeriod} days was the longest orbital period found!`);
